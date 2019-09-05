8b7d0d7b38fce4023bbfe5a8a54ee24ccd20aa1b
Behdad Nemati <behdad.nemati1@gmail.com>
2019-09-05 23:01:14 +0430

    Add project files.

---
+++ WebApplication3_Session/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3_Session.Models;
+
+namespace WebApplication3_Session.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult InsertSession(string value)
+        {
+            HttpContext.Session.SetString("value", value);
+            ViewData["msg"] = "Stored in Session";
+            return View("Index");
+        }
+        public IActionResult ReadSession()
+        {
+            //HttpContext.Request
+            //HttpContext.Response
+            var session = HttpContext.Session.GetString("value");
+            if (session == null)
+                ViewData["msg"] = "No Session or expired";
+            else
+                ViewData["msg"] = session;
+            return View("Index");
+
+            //Important note: please remove a unusec variable or clear whole session memory when you do not need immidiately
+            //هروقت حافظه سیششن را نخواستید ، سریع حافظه خالی بفرمایید
+            //HttpContext.Session.Remove("value");
+            //HttpContext.Session.Clear();
+        }
+
+    }
+}
---
+++ WebApplication3_Session/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplication3_Session.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
